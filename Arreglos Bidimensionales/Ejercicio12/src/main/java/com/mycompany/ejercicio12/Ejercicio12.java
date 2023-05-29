1092f0a7b731766a0722a3f079e0846b53a144ca
Mariangel Rojas <marojas31@utpl.edu.ec>
2023-05-29 00:55:43 -0500

    Ejercicios con arreglos

---
+++ Arreglos Bidimensionales/Ejercicio12/src/main/java/com/mycompany/ejercicio12/Ejercicio12.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.ejercicio12;
+
+import java.util.Scanner;
+
+/* 12. Dada una matriz de tamaño NxN (impar), realice un método que me permita mostrar un cuadro
+de asteriscos (solo bordes)*/
+public class Ejercicio12 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingrese el tamaño de la matriz (impar): ");
+        int a = entrada.nextInt();
+
+        if (a % 2 != 0) {
+            cuadroA(a);
+        } else {
+            System.out.println("El tamaño de la matriz debe ser impar.");
+        }
+    }
+
+    public static void cuadroA(int b) {
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < b; j++) {
+                if (i == 0 || i == b - 1 || j == 0 || j == b - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ Arreglos Unidimensionales/Ejercicio02/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.ejercicio2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*2. Escriba un programa que me permita almacenar en un arreglo B, el factorial de 10 
+    números almacenados previamente en un arreglo A.*/
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int[] arrA = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arrB = new int[10];
+
+        for (int i = 0; i < arrA.length; i++) {
+            arrB[i] = factorial(arrA[i]);
+        }
+
+        System.out.println("El arreglo A es: " + Arrays.toString(arrA));
+        System.out.println("El arreglo B es: " + Arrays.toString(arrB));
+
+    }
+
+    public static int factorial(int num) {
+        if (num == 1) {
+            return num;
+        } else {
+            return num * factorial(num - 1);
+        }
+
+    }
+}
